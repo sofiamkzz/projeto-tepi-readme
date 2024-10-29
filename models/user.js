d9e161774a817874590a67f317d020e711fd6ac5
sofiamkzz <124911331+sofiamkzz@users.noreply.github.com>
2024-10-29 15:48:27 -0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,23 @@
+const { DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/database');
 
+// Definição do modelo de usuário
+const User = sequelize.define('User', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
