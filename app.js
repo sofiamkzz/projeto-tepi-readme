368439aa2313e11b4a8716c6f6ba21bec549d204
sofiamkzz <124911331+sofiamkzz@users.noreply.github.com>
2024-11-16 23:43:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,28 +5,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const path = require('path');
+
 const userRoutes = require('./routes/userRoutes'); // Importando rotas de usuário
 const authRoutes = require('./routes/authRoutes'); // Importando rotas de autenticação
 const cartRoutes = require('./routes/cartRoutes'); // Importando rotas de carrinho
-const { isAdmin } = require('./middleware/isAdmin'); // Middleware para verificar admin, se necessário
+
+const authenticateToken = require('./middleware/auth');
 
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.set('view engine', 'ejs');
-
-// Middlewares para parsear dados do corpo das requisições
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
 
 // Middleware para sessões
 app.use(session({
     secret: process.env.SESSION_SECRET || 'your_default_secret_key', // Use uma variável de ambiente
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false } // Defina como true se estiver usando HTTPS
+    cookie: { secure: true }
 }));
 
 sequelize.sync({ force: true }).then(() => {
