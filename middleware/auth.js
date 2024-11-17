5fc6c0833e07f3745ea08e4f20126a34d1082e9f
sofiamkzz <124911331+sofiamkzz@users.noreply.github.com>
2024-11-16 23:13:41 -0300

    Create auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const jwt = require('jsonwebtoken');
+const secret = process.env.SECRET;
+
+// Middleware de autenticação
+const authenticateToken = (req, res, next) => {
+    const token = req.headers['authorization'];
+
+    if (!token) {
+        return res.status(401).json({ error: 'Token não fornecido.' });
+    }
+
+    jwt.verify(token, secret, (err, user) => {
+        if (err) {
+            return res.status(403).json({ error: 'Token inválido.' });
+        }
+
+        req.user = user;
+        next();
+    });
+};
+
+module.exports = authenticateToken;
