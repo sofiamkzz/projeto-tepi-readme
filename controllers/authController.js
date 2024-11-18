33bb5a47de814e79b08363869d5b5d0fbcdb420e
sofiamkzz <124911331+sofiamkzz@users.noreply.github.com>
2024-11-18 10:37:37 -0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,13 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
-// Render the login page
+// Renderiza a página de login
 const getLoginPage = (req, res) => {
     res.render('login', { mensagem: '' });
 };
 
-// Handle user login
+// Função de login
 const loginUser = async (req, res) => {
     const { email, password } = req.body;
 
@@ -15,7 +16,7 @@ const loginUser = async (req, res) => {
 
         if (user) {
             if (await bcrypt.compare(password, user.password)) {
-                const token = jwt.sign(user.email, process.env.SECRET);
+                const token = jwt.sign({ id: user.id, email: user.email }, process.env.SECRET, { expiresIn: '1h' });
                 req.session.userId = user.id;
                 res.render('conta', { user: user, token: token });
             }
@@ -24,21 +25,21 @@ const loginUser = async (req, res) => {
             return res.render('login', { mensagem: 'Usuário não encontrado.' });
         }
 
-        req.session.userId = user.id;
-        res.render('conta', { user });
     } catch (error) {
         console.error('Erro no login:', error);
         res.render('login', { mensagem: 'Erro ao fazer login.' });
     }
 };
 
-// Handle user logout
+// Função para deslogar usuário
 const logoutUser = async (req, res) => {
     req.session.destroy((err) => {
         if (err) {
             console.error('Erro ao deslogar:', err);
             return res.redirect('/');
         }
+
+        res.clearCookie('connect.sid');
         res.render('login', { mensagem: 'Usuário deslogado com sucesso!' });
     });
 };
