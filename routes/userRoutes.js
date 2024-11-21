50a8b5a4ce65ba846cd058c9195969b297698be1
sofiamkzz <124911331+sofiamkzz@users.noreply.github.com>
2024-11-21 15:48:10 -0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,18 @@ const express = require('express');
 const { createUser, updateUser, deleteUserById } = require('../controllers/userController');
 const router = express.Router();
 
+// Registration page (GET)
+router.get('/cadastro', (req, res) => {
+    res.render('cadastro', { mensagem: '' });
+});
+
+// Create user (POST)
 router.post('/cadastro', createUser);
+
+// Update user by ID (PUT)
 router.put('/atualizar/:id', updateUser);
+
+// Delete user by ID (DELETE)
 router.delete('/delete/:userId', deleteUserById);
 
 module.exports = router;
