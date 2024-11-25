a23ca003947447e19ac544286ac6721d94bee700
sofiamkzz <124911331+sofiamkzz@users.noreply.github.com>
2024-11-25 13:29:37 -0300

    Update pedido.js

--- models/pedido.js
+++ models/pedido.js
@@ -1,8 +1,14 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
+const User = require('./user');
 
 // Definição do modelo de Pedido
 const Order = sequelize.define('Order', {
+  idOrder: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
   status: {
     type: DataTypes.STRING,
     allowNull: false,
