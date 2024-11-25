ed515a65c53792508d386fb54182805df5e1637d
sofiamkzz <124911331+sofiamkzz@users.noreply.github.com>
2024-11-25 13:29:24 -0300

    Update item-pedido.js

--- models/item-pedido.js
+++ models/item-pedido.js
@@ -1,8 +1,15 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
+const Product = require('./produto');
+const Order = require('./pedido');
 
 // Definição do modelo de ItemPedido
 const OrderItem = sequelize.define('OrderItem', {
+  idOrderItem: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
   quantity: {
     type: DataTypes.INTEGER,
     allowNull: false,
