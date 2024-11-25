a970985891cc53a7ce827029607b0a78c9232fbd
sofiamkzz <124911331+sofiamkzz@users.noreply.github.com>
2024-11-25 13:28:37 -0300

    Update categoria.js

--- models/categoria.js
+++ models/categoria.js
@@ -3,6 +3,11 @@ const sequelize = require('../config/database');
 
 // Definição do modelo de Categoria
 const Category = sequelize.define('Category', {
+  idCategory: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
   name: {
     type: DataTypes.STRING,
     allowNull: false,
