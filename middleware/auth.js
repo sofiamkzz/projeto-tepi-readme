ef14734103d500a385a789f16a63a67b09ea0c2b
sofiamkzz <124911331+sofiamkzz@users.noreply.github.com>
2024-11-28 15:45:03 -0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,35 +1,23 @@
 require('dotenv').config();
+
 const jwt = require('jsonwebtoken');
 const secret = process.env.SECRET;
 
 // Middleware de autenticação
 const authenticateToken = (req, res, next) => {
-    // Verificar se o token está no cabeçalho de autorização
-    const authHeader = req.headers['authorization'];
-    console.log('Authorization Header:', authHeader);  // Log do cabeçalho Authorization
-
-    if (!authHeader) {
-        return res.status(401).json({ error: 'Token não fornecido. Autorização necessária.' });
-    }
-
-    // O token é esperado no formato "Bearer <token>"
-    const token = authHeader.split(' ')[1]; // Pega a parte após o "Bearer"
-    console.log('Token extraído:', token);  // Log do token extraído
+    const token = req.headers['authorization'];
 
     if (!token) {
-        return res.status(401).json({ error: 'Token mal formatado. Por favor, use o formato "Bearer <token>".' });
+        return res.status(401).json({ error: 'Token não fornecido.' });
     }
 
-    // Verificação do token
     jwt.verify(token, secret, (err, user) => {
         if (err) {
-            console.log('Erro na verificação do token:', err);  // Log do erro de verificação
-            return res.status(403).json({ error: 'Token inválido. Acesso proibido.' });
+            return res.status(403).json({ error: 'Token inválido.' });
         }
 
-        console.log('Usuário autenticado:', user);  // Log do usuário autenticado
-        req.user = user; // O usuário é atribuído à requisição para ser usado nas rotas
-        next(); // Chama o próximo middleware ou rota
+        req.user = user;
+        next();
     });
 };
 
