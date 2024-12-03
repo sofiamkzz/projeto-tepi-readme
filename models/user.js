14f0107c2d22a85e3ee27abaeb6cc35095e5442c
sofiamkzz <124911331+sofiamkzz@users.noreply.github.com>
2024-12-03 15:33:08 -0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,41 +1,45 @@
 const { DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/database');
+const CartItem = require('./item-carrinho');  // Verifique o caminho aqui
 
 // Definição do modelo de usuário
 const User = sequelize.define('User', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  role: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    defaultValue: 'user', 
-    validate: {
-      isIn: [['user', 'admin']]  
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: 'user',
+        validate: {
+            isIn: [['user', 'admin']]
+        }
     }
-  }
 });
 
 // Hook para hash da senha antes de salvar o usuário no banco de dados
 User.beforeCreate(async (user, options) => {
-  const salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(user.password, salt);
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
 });
+
+// Relacionamento: Um usuário pode ter vários itens no carrinho
+User.hasMany(CartItem, { foreignKey: 'userId' });
 
 module.exports = User;
