ee6767b2310af68358d9e7076b9be9ca4400d973
sofiamkzz <124911331+sofiamkzz@users.noreply.github.com>
2024-12-03 15:33:35 -0300

    Update cartRoutes.js

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -2,8 +2,13 @@ const express = require('express');
 const { getCart, addToCart, removeFromCart } = require('../controllers/cartController');
 const router = express.Router();
 
+// Rota para exibir o carrinho
 router.get('/carrinho', getCart);
-router.post('/carrinho/add', addToCart);
-router.delete('/carrinho/remove/:cartItemId', removeFromCart);
+
+// Rota para adicionar um produto ao carrinho
+router.get('/carrinho/adicionar/:id', addToCart);
+
+// Rota para remover um produto do carrinho
+router.get('/carrinho/remover/:id', removeFromCart);
 
 module.exports = router;
