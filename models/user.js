659d1e34a52e8c03411250c90829f2d84e33df67
sofiamkzz <124911331+sofiamkzz@users.noreply.github.com>
2024-12-12 16:14:34 -0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -8,6 +8,10 @@ const User = sequelize.define('User', {
     primaryKey: true,
     autoIncrement: true,
   },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,  
+  },
   email: {
     type: DataTypes.STRING,
     allowNull: false,
