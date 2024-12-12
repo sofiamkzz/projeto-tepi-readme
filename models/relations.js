67dc69c7677470a2e0e7709b0133da7ffe9a8e4f
sofiamkzz <124911331+sofiamkzz@users.noreply.github.com>
2024-12-12 15:14:37 -0300

    Create relations.js

---
+++ models/relations.js
@@ -0,0 +1,35 @@
+const User = require('./user');
+const Product = require('./product');
+const Cart = require('./cart');
+const CartItem = require('./cartItem');
+const Category = require('./category');
+const Order = require('./order');
+const Donation = require('./donation');
+const Favorite = require('./favorite');
+
+User.hasMany(Cart, { foreignKey: 'userId' });
+User.hasMany(Order, { foreignKey: 'userId' });
+User.hasMany(Donation, { foreignKey: 'userId' });
+User.hasMany(Favorite, { foreignKey: 'userId' });
+
+Product.hasMany(CartItem, { foreignKey: 'productId' });
+Product.belongsTo(Category, { foreignKey: 'categoryId' });
+Product.hasMany(Favorite, { foreignKey: 'productId' });
+
+Category.hasMany(Product, { foreignKey: 'categoryId' });
+
+Cart.belongsTo(User, { foreignKey: 'userId' });
+Cart.hasMany(CartItem, { foreignKey: 'cartId' });
+
+CartItem.belongsTo(Cart, { foreignKey: 'cartId' });
+CartItem.belongsTo(Product, { foreignKey: 'productId' });
+
+Order.belongsTo(User, { foreignKey: 'userId' });
+Order.belongsTo(Cart, { foreignKey: 'cartId' });
+Order.hasMany(Donation, { foreignKey: 'orderId' });
+
+Donation.belongsTo(User, { foreignKey: 'userId' });
+Donation.belongsTo(Order, { foreignKey: 'orderId' });
+
+Favorite.belongsTo(User, { foreignKey: 'userId' });
+Favorite.belongsTo(Product, { foreignKey: 'productId' });
